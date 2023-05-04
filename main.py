07c0ea9016fb060c772e58e7eb26b07d892d07f0
jacob-macleod <jacob.e.macleod@gmail.com>
2023-05-04 08:04:04 +0100

    Updated algorithm to compare words against list

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import csv
+import csv, re
 
 words = []
 definitions = []
 mostCommonWords = []
 mostCommonDefinitions = []
+
+vowel_pattern = re.compile(r"[\u0591-\u05C7]")
+
+# Remove vowels from the word when comparing it against the most common words
+def removeVowels(text: str) -> str:
+    return re.sub(vowel_pattern, "", text)
 
 # Store the most common words and definitions from the csv file
 with open ("1000MostCommon.csv", "r") as csvfile:
@@ -15,6 +21,7 @@ with open ("1000MostCommon.csv", "r") as csvfile:
 # Open the file
 with open("demo.tex", "r") as file:
     for line in file:
+        wordUsed = False
         # If the line has a volcabulary word
         if "{\hebrewfont\RL{" in line:
             # Get the word
@@ -26,7 +33,11 @@ with open("demo.tex", "r") as file:
             # If the word isn't in the most common words
             # This works even if the descriptions are different, which may happen, or if two
             # words have the same description but are different words
-            if word not in mostCommonWords:
+            for mostCommonWord in mostCommonWords:
+                if removeVowels(word) == removeVowels(mostCommonWord):
+                    wordUsed = True
+            
+            if wordUsed == False:
                 definitions.append(definition)
                 words.append(word)
 
